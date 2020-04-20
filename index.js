7b2b44121beae347bb4f8003c57025d4818c425a
M Thariq Nugroho <thariq1902@gmail.com>
2020-04-20 10:19:25 +0700

    Update .gitignore, cert.pem, and 3 more files...

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var helmet = require("helmet");
+var cors = require("cors");
+var fs = require("fs");
+var multer = require("multer");
+var https = require("https");
+var http = require("http");
+var PORT = 5000;
+var SPORT = 5443;
+
+/**
+ * Middleware
+ */
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+const storage = multer.diskStorage({
+  destination: (request, file, callback) => {
+    callback(null, "/var/spool/recording/");
+  },
+  filename: (request, file, callback) => {
+    console.log(file);
+    callback(null, file.originalname);
+  },
+});
+var upload = multer({ storage: storage }).single("videoFile");
+
+/**
+ * Endpoints
+ */
+app.get("/", async (req, res) => {
+  res.status(200).send("Hello");
+});
+
+app.get("/do/upload", async (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.error("error occured");
+      res.status(500).json({ code: 0, status: "Error Occured" });
+      return;
+    }
+    console.log(req.file);
+    console.log("Video Uploaded");
+    res.status(200).json({ code: 1, status: "Video Uploaded" });
+  });
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("./cert/privkey.pem"),
+      cert: fs.readFileSync("./cert/cert.pem"),
+      passphrase: "mitrak",
+    },
+    app
+  )
+  .listen(SPORT);
+http.createServer(app).listen(PORT);
