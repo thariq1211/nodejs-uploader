fb348c5dd8557f8bbd305b8f104294b5c8aa8230
M Thariq Nugroho <thariq1902@gmail.com>
2020-05-08 12:50:18 +0700

    Update combine endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var helmet = require("helmet");
 var cors = require("cors");
 var fs = require("fs");
+var { exec } = require("child_process");
 var multer = require("multer");
 var https = require("https");
 var http = require("http");
@@ -33,18 +34,56 @@ app.get("/", async (req, res) => {
   res.status(200).send("Hello");
 });
 
-app.post("/system/upload", async (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      console.error("error occured");
+app.post(
+  "/system/upload/agent/:agent/nik/:nik/date/:date/time/:time",
+  async (req, res) => {
+    const { agent, nik, date, time } = req.params;
+    if (!agent && !nik && !date && !time) {
       res.status(500).json({ code: 0, status: "Error Occured" });
       return;
     }
-    console.log(req.file);
-    console.log("Video Uploaded");
-    res.status(200).json({ code: 1, status: "Video Uploaded" });
-  });
-});
+    upload(req, res, (err) => {
+      if (err) {
+        console.error("error occured");
+        res.status(500).json({ code: 0, status: "Error Occured" });
+        return;
+      }
+      console.log(req.file);
+      console.log("Video Uploaded");
+      exec(
+        `bash doCombine IN-AGENT${agent}-NIK:${nik}-Date:${date}-Time:${time} IN-AGENT*-NIK:${nik}-Date:${date}-Time:${time} IN-AGENT${agent}-NIK:${nik}-Date:${date}-Time:${time}`,
+        (error, stderr, stdout) => {
+          console.log("try to combine video and audio");
+          if (error) {
+            console.log(error);
+            res.json({
+              status: 0,
+              message: "Successful Upload, but combine error!!",
+              output: error,
+            });
+            return;
+          }
+          if (stderr) {
+            console.log(stderr);
+            res.json({
+              status: 1,
+              message: "Successful Upload, combine with output message!!",
+              output: stderr,
+            });
+            return;
+          }
+          console.log(stdout);
+          res.json({
+            status: 1,
+            message: "Successful Upload, combine success!!",
+            output: stdout,
+          });
+          return;
+        }
+      );
+    });
+  }
+);
 
 /**Uncomment code ini jika ingin ditambah ssl */
 /**
