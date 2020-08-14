49f857f7ddce636ae83b492595203cb92662461e
thoriq <thoriqnugroho3@gmail.com>
2020-08-14 17:00:49 +0700

    update index.js

--- helpers/multer.js
+++ helpers/multer.js
@@ -2,6 +2,12 @@ var multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (_request, _file, callback) => {
+    console.log({
+      type: _file.mimetype,
+      file: _file.filename,
+      size: _file.size,
+      path: _file.path
+    })
     callback(null, "/var/spool/recording/");
   },
   filename: (_request, _file, callback) => {
